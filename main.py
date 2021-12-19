920d8036b2b81684fea93cfa3be74d8d49ebe8a4
Hayden <asianhaydenxd@gmail.com>
2021-12-19 15:18:05 -0500

    Add basic functioning grapher

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
 import turtle as trtl
+
+XMIN = -20
+XMAX = 20
+YMIN = -20
+YMAX = 20
+
+expression = input("y=")
+
+grapher = trtl.Turtle()
+grapher.penup()
+grapher.speed(11)
+
+for x in range(XMIN, XMAX+1):
+    substituted_expression = expression.replace("x", f"({str(x)})")
+    y = eval(substituted_expression)
+    
+    grapher.goto(x*20, y*20)
+    print(f"{x}:{y}")
+    grapher.pendown()
+
+trtl.done()
