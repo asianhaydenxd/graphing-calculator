e3c1d679e90e4a3153378588ca5f5f9d8d85456a
Hayden <asianhaydenxd@gmail.com>
2021-12-19 15:46:39 -0500

    Add comments

--- main.py
+++ main.py
@@ -14,18 +14,23 @@ DEFAULT_EXPRESSION = "15/x"
 
 expression = input("y=") if TAKE_INPUT else DEFAULT_EXPRESSION
 
+# Initialize turtle "grapher" and make as fast as possible
 grapher = trtl.Turtle()
 grapher.penup()
 grapher.speed(11)
 
 for x in range(XMIN, XMAX+1):
+    # Substitute the "x" with the current x (input, independent) value
     substituted_expression = expression.replace("x", f"({str(x)})")
+    
+    # Get the y (output, dependent) value; skip if undefined
     try:
         y = eval(substituted_expression)
     except ZeroDivisionError:
         grapher.penup()
         continue
     
+    # Go to the next step
     grapher.goto(x*20, y*20)
     print(f"{x}:{y}")
     grapher.pendown()
