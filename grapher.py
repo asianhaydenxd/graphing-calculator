344bdd4a0f25bd28d66c6bff3b197f0245a73c48
Hayden <asianhaydenxd@gmail.com>
2021-12-21 08:56:10 -0500

    Make width, height, and plot boundaries readonly

--- grapher.py
+++ grapher.py
@@ -4,8 +4,8 @@ from math import *
 class Grapher:
     def __init__(self, width=500, height=500, xmin=-10, xmax=10, ymin=-10, ymax=10):
         # Set dimensions
-        self.width = width
-        self.height = height
+        self._width = width
+        self._height = height
         
         # Initialize screen and screen dimensions
         self.wn = trtl.Screen()
@@ -18,20 +18,20 @@ class Grapher:
         self.grapher.hideturtle()
         
         # Initialize plot boundaries
-        self.xmin = xmin
-        self.xmax = xmax
-        self.ymin = ymin
-        self.ymax = ymax
+        self._xmin = xmin
+        self._xmax = xmax
+        self._ymin = ymin
+        self._ymax = ymax
         
         # Locates the origin (0,0) of the graph
-        self.x_center = (-(self.xmin + self.xmax)/2) * (self.width/(self.xmax-self.xmin))
-        self.y_center = (-(self.ymin + self.ymax)/2) * (self.height/(self.ymax-self.ymin))
+        self.x_center = (-(self._xmin + self._xmax)/2) * (self._width/(self._xmax-self._xmin))
+        self.y_center = (-(self._ymin + self._ymax)/2) * (self._height/(self._ymax-self._ymin))
         
         self.draw_axes()
     
     # Gets the X value of the graph that the given X coordinate represents
     def get_value(self, x_coord):
-        return (x_coord-self.width/2) / (self.width/(self.xmax-self.xmin)) + (self.xmin + self.xmax)/2
+        return (x_coord-self._width/2) / (self._width/(self._xmax-self._xmin)) + (self._xmin + self._xmax)/2
     
     # Must be placed after all uses of the grapher
     def done(self):
@@ -45,16 +45,16 @@ class Grapher:
 
         # Draws the X axis
         self.grapher.goto(self.x_center, self.y_center)
-        self.grapher.back(self.grapher.xcor() + self.width/2)
+        self.grapher.back(self.grapher.xcor() + self._width/2)
         self.grapher.pendown()
-        self.grapher.forward(self.width)
+        self.grapher.forward(self._width)
 
         # Draws the Y axis
         self.grapher.goto(self.x_center, self.y_center)
         self.grapher.setheading(90)
-        self.grapher.back(self.grapher.ycor() + self.height/2)
+        self.grapher.back(self.grapher.ycor() + self._height/2)
         self.grapher.pendown()
-        self.grapher.forward(self.height)
+        self.grapher.forward(self._height)
         self.grapher.penup()
     
     # Draws the given expression on the graph
@@ -64,7 +64,7 @@ class Grapher:
         self.grapher.pensize(2)
 
         # For every X coordinate that is going to be checked
-        for col in range(int(self.width/precision)):
+        for col in range(int(self._width/precision)):
             x_coord = col * precision
             x = self.get_value(x_coord)
             
@@ -76,19 +76,19 @@ class Grapher:
                 continue
             
             # Go all the way to the top if the graph goes above the height range
-            if y > self.ymax:
-                self.grapher.goto((x_coord-1-self.width/2), self.height/2)
+            if y > self._ymax:
+                self.grapher.goto((x_coord-1-self._width/2), self._height/2)
                 self.grapher.penup()
                 continue
             
             # Go all the way to the bottom if the graph goes below the height range
-            if y < self.ymin:
-                self.grapher.goto((x_coord-1-self.width/2), -self.height/2)
+            if y < self._ymin:
+                self.grapher.goto((x_coord-1-self._width/2), -self._height/2)
                 self.grapher.penup()
                 continue
             
             # Go to the next X value
-            self.grapher.goto((x_coord-self.width/2), (y/(self.ymax-self.ymin)*self.height))
+            self.grapher.goto((x_coord-self._width/2), (y/(self._ymax-self._ymin)*self._height))
             self.grapher.pendown()
         
         # Stop drawing at the end of the graph
